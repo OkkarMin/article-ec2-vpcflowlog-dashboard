a1b484c675a2f900c3f457b69a708db140cf2c87
OkkarMin <OMIN001@e.ntu.edu.sg>
2022-11-27 15:43:07 +0800

    feat: add opensearch and lambda to stream logs

--- lib/article-ec2-vpcflowlog-dashboard-stack.ts
+++ lib/article-ec2-vpcflowlog-dashboard-stack.ts
@@ -1,13 +1,28 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { aws_ec2 as ec2, aws_s3 as s3 } from "aws-cdk-lib";
+import {
+  aws_ec2 as ec2,
+  aws_s3 as s3,
+  aws_lambda as lambda,
+  aws_lambda_event_sources as lambdaEventSources,
+  aws_opensearchservice as opensearch,
+} from "aws-cdk-lib";
+
+import * as path from "path";
+
+const OPENSEARCH_USER = process.env.OPENSEARCH_USER as string;
+const OPENSEARCH_PASSWORD = process.env.OPENSEARCH_PASSWORD as string;
+if (!OPENSEARCH_USER || !OPENSEARCH_PASSWORD) {
+  throw new Error("OPENSEARCH_USER and OPENSEARCH_PASSWORD must be set");
+}
 
 export class ArticleEc2VpcflowlogDashboardStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const s3Bucket = new s3.Bucket(this, "S3Bucket", {
+    const vpcFlowLogs3Bucket = new s3.Bucket(this, "S3Bucket", {
       bucketName: "article-ec2-vpcflowlog-dashboard-s3bucket",
+      autoDeleteObjects: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
@@ -54,5 +69,56 @@ export class ArticleEc2VpcflowlogDashboardStack extends cdk.Stack {
         ),
       }
     );
+
+    const opensearchDomain = new opensearch.Domain(this, "Domain", {
+      version: opensearch.EngineVersion.OPENSEARCH_1_3,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      enforceHttps: true,
+      nodeToNodeEncryption: true,
+      encryptionAtRest: {
+        enabled: true,
+      },
+      fineGrainedAccessControl: {
+        masterUserName: OPENSEARCH_USER,
+        masterUserPassword:
+          cdk.SecretValue.unsafePlainText(OPENSEARCH_PASSWORD),
+      },
+    });
+
+    const gzipToCSVFunction = new lambda.Function(
+      this,
+      "ArticleEc2VpcflowlogDashboardGzipToCSVFunction",
+      {
+        code: lambda.Code.fromAsset(path.join(__dirname, "lambda"), {
+          bundling: {
+            image: lambda.Runtime.PYTHON_3_9.bundlingImage,
+            user: "root",
+            command: [
+              "bash",
+              "-c",
+              "pip install -r requirements.txt -t . && cp -r . /asset-output",
+            ],
+          },
+        }),
+        runtime: lambda.Runtime.PYTHON_3_9,
+        handler: "gzip_to_csv.handler",
+        environment: {
+          OPENSEARCH_HOST: opensearchDomain.domainEndpoint,
+          OPENSEARCH_INDEX: "vpcflowlog",
+          OPENSEARCH_USER,
+          OPENSEARCH_PASSWORD,
+        },
+      }
+    );
+
+    const s3PutEventSource = new lambdaEventSources.S3EventSource(
+      vpcFlowLogs3Bucket,
+      {
+        events: [s3.EventType.OBJECT_CREATED],
+      }
+    );
+    vpcFlowLogs3Bucket.grantRead(gzipToCSVFunction);
+    gzipToCSVFunction.addEventSource(s3PutEventSource);
+    opensearchDomain.grantWrite(gzipToCSVFunction);
   }
 }
---
+++ lib/lambda/gzip_to_csv.py
@@ -0,0 +1,74 @@
+import boto3
+import os
+import gzip
+from opensearchpy import OpenSearch
+from datetime import datetime
+import pytz
+
+region = "ap-southeast-1"
+service = "es"
+auth = (os.environ.get("OPENSEARCH_USER"), os.environ.get("OPENSEARCH_PASSWORD"))
+opensearch_host = os.environ.get("OPENSEARCH_HOST")
+index = os.environ.get("OPENSEARCH_INDEX")
+type = "_doc"
+url = f"https://{opensearch_host}/{index}/{type}"
+headers = {"Content-Type": "application/json"}
+
+s3 = boto3.client("s3")
+
+opensearch_client = OpenSearch(
+    hosts=[{"host": opensearch_host, "port": 443}],
+    http_auth=auth,
+    http_compress=True,
+    use_ssl=True,
+    verify_certs=True,
+    ssl_show_warn=False,
+)
+
+
+def handler(event, _) -> None:
+    for record in event["Records"]:
+        bucket = record["s3"]["bucket"]["name"]
+        key = record["s3"]["object"]["key"]
+        file_name = key.split("/")[-1]
+
+        temp_file = f"/tmp/{file_name}"
+        s3.download_file(Bucket=bucket, Key=key, Filename=temp_file)
+
+        with gzip.open(temp_file, "rb") as f:
+            for line in f:
+                data = line.decode("utf-8").split()
+
+                if data[0] == "version" or data[3] == "10.0.0.14":
+                    continue
+
+                date = datetime.fromtimestamp(
+                    int(data[10]), pytz.timezone("Asia/Singapore")
+                ).strftime("%Y-%m-%dT%H:%M:%S")
+
+                document = {
+                    "version": data[0],
+                    "account_id": data[1],
+                    "interface_id": data[2],
+                    "ip_address": data[3],  # for opensearch to recognize
+                    "src_addr": data[3],
+                    "dst_addr": data[4],
+                    "src_port": data[5],
+                    "dst_port": data[6],
+                    "protocol": data[7],
+                    "packets": data[8],
+                    "bytes": data[9],
+                    "start": data[10],
+                    "end": data[11],
+                    "action": data[12],
+                    "log_status": data[13],
+                    "date": date,
+                }
+
+                response = opensearch_client.index(
+                    index=index,
+                    body=document,
+                    refresh=True,  # refresh the index to make the document available for search
+                )
+
+                print(response)
